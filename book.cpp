ef2236d63350dada21ff30502e12b3692646dd88
raymond-esparza <47377280+raymond-esparza@users.noreply.github.com>
2019-04-16 22:45:03 -0700

    Add files via upload

---
+++ book.cpp
@@ -0,0 +1,33 @@
+//book.cpp
+#include "book.h"
+
+Book::Book(int id, string bookName, string auth, string cat) {
+    bookID = id;
+    title = bookName;
+    author = auth;
+    category = cat;
+}
+
+string Book::getTitle() {
+    return title;
+}
+
+string Book::getAuthor() {
+    return author;
+}
+
+string Book::getCategory() {
+    return category;
+}
+
+int Book::getId() {
+    return bookID;
+}
+void Book::setPersonPtr(Person * ptr) {
+	personPtr = ptr;
+	return;
+}
+
+Person * Book::getPersonPtr() {
+    return personPtr; // complete
+}
